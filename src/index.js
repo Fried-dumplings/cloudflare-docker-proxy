690d1a9b767345e1b5b211e4f5964c5053c83e66
Fried-dumplings <57983815+Fried-dumplings@users.noreply.github.com>
2024-06-08 23:06:19 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.d.zoan.top": "https://registry-1.docker.io",
+  "quay.d.zoan.top": "https://quay.io",
+  "gcr.d.zoan.top": "https://gcr.io",
+  "k8s-gcr.d.zoan.top": "https://k8s.gcr.io",
+  "k8s.d.zoan.top": "https://registry.k8s.io",
+  "ghcr.d.zoan.top": "https://ghcr.io",
+  "cloudsmith.d.zoan.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
